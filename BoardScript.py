1047909481a85e96f305f818298960e851ea558b
jake <jakubkuba997@gmail.com>
2019-12-12 20:01:32 +0100

    Player can now move and keep track of their score. Added bonuses to a board.

--- BoardScript.py
+++ BoardScript.py
@@ -1,15 +1,21 @@
 
 """
-    Create and draw a playable maze
+    Create, draw a playable maze and move around it
 """
 
 
 class Board:
-    def __init__(self, dimensions: int = 10, obstacles: int = 2, max_learning_value: int = 100) -> None:
+    def __init__(self, dimensions: int = 10, obstacles: int = 2, disappearing_obstacles: bool = False, bonuses: int = 2, bonus_value: int = 10, reward_value: int = 100) -> None:
         self.dimensions = dimensions
         self.obstacles = obstacles
-        self.max_learning_value = max_learning_value
-        self.board = []     # column, row, trophy, player, obstacle, worth
+        self.disappearing_obstacles = disappearing_obstacles
+        self.bonuses = bonuses
+        self.reward_value = reward_value
+        self.bonus_value = bonus_value
+        self.player_coordinates = []
+        self.number_of_moves = 0
+        self.total_score = 0
+        self.board = []     # column, row, trophy, player, obstacle, bonus, worth
 
     def initialize_board(self) -> None:
         self.create_board()
@@ -20,7 +26,7 @@ class Board:
         for y in range(self.dimensions):
             row = []
             for x in range(self.dimensions):
-                row.append([x, y, 0, 0, 0, 0])
+                row.append([x, y, 0, 0, 0, 0, 0])
             self.board.append(row)
 
     def add_objects(self) -> None:
@@ -35,31 +41,69 @@ class Board:
             while coordinates == trophy_coordinates or coordinates == player_coordinates or coordinates in obstacle_coordinates:
                 coordinates = [rd(0, self.dimensions - 1), rd(0, self.dimensions - 1)]
             obstacle_coordinates.append(coordinates)
+        bonuses_coordinates = []
+        for _ in range(self.bonuses):
+            coordinates = [rd(0, self.dimensions - 1), rd(0, self.dimensions - 1)]
+            while coordinates == trophy_coordinates or coordinates == player_coordinates or coordinates in obstacle_coordinates or coordinates in bonuses_coordinates:
+                coordinates = [rd(0, self.dimensions - 1), rd(0, self.dimensions - 1)]
+            bonuses_coordinates.append(coordinates)
+        self.player_coordinates = player_coordinates
         self.board[trophy_coordinates[1]][trophy_coordinates[0]][2] = 1
         self.board[player_coordinates[1]][player_coordinates[0]][3] = 1
         for coordinate in obstacle_coordinates:
             self.board[coordinate[1]][coordinate[0]][4] = 1
+        for coordinate in bonuses_coordinates:
+            self.board[coordinate[1]][coordinate[0]][5] = 1
 
     def actualise_worth(self) -> None:
         for row_index, row in enumerate(self.board):
             for column_index, value in enumerate(row):
                 if value[2] == 1:
-                    self.board[row_index][column_index][5] = self.max_learning_value
+                    self.board[row_index][column_index][6] = self.reward_value
                 elif value [4] == 1:
-                    self.board[row_index][column_index][5] = -self.max_learning_value
+                    self.board[row_index][column_index][6] = -self.reward_value
+                elif value [5] == 1:
+                    self.board[row_index][column_index][6] = self.bonus_value
 
     def draw_board(self) -> None:
         spaces = '\t'
         for row in self.board:
             print(spaces, end='')
             for value in row:
-                if value[2] == 1:
-                    print('T', end='')
-                elif value[3] == 1:
+                if value[3] == 1:
                     print('P', end='')
+                elif value[2] == 1:
+                    print('T', end='')
                 elif value[4] == 1:
                     print('O', end='')
+                elif value[5] == 1:
+                    print('x', end='')
                 else:
                     print('.', end='')
                 print(spaces, end='')
             print()
+
+    def move(self, direction: str) -> None:
+        if direction == 'up' and self.player_coordinates[1] != 0:
+            self.board[self.player_coordinates[1]][self.player_coordinates[0]][3] = 0
+            self.board[self.player_coordinates[1] - 1][self.player_coordinates[0]][3] = 1
+            self.player_coordinates = [self.player_coordinates[0], self.player_coordinates[1] - 1]
+        elif direction == 'down' and self.player_coordinates[1] != self.dimensions - 1:
+            self.board[self.player_coordinates[1]][self.player_coordinates[0]][3] = 0
+            self.board[self.player_coordinates[1] + 1][self.player_coordinates[0]][3] = 1
+            self.player_coordinates = [self.player_coordinates[0], self.player_coordinates[1] + 1]
+        elif direction == 'left' and self.player_coordinates[0] != 0:
+            self.board[self.player_coordinates[1]][self.player_coordinates[0]][3] = 0
+            self.board[self.player_coordinates[1]][self.player_coordinates[0] - 1][3] = 1
+            self.player_coordinates = [self.player_coordinates[0] - 1, self.player_coordinates[1]]
+        elif direction == 'right' and self.player_coordinates[0] != self.dimensions - 1:
+            self.board[self.player_coordinates[1]][self.player_coordinates[0]][3] = 0
+            self.board[self.player_coordinates[1]][self.player_coordinates[0] + 1][3] = 1
+            self.player_coordinates = [self.player_coordinates[0] + 1, self.player_coordinates[1]]
+        self.total_score += self.board[self.player_coordinates[1]][self.player_coordinates[0]][6]
+        if self.board[self.player_coordinates[1]][self.player_coordinates[0]][5] == 1:
+            self.board[self.player_coordinates[1]][self.player_coordinates[0]][5] = 0
+        if self.disappearing_obstacles and self.board[self.player_coordinates[1]][self.player_coordinates[0]][4] == 1:
+            self.board[self.player_coordinates[1]][self.player_coordinates[0]][4] = 0
+        self.number_of_moves += 1
+        self.actualise_worth()
--- MainScript.py
+++ MainScript.py
@@ -8,6 +8,13 @@ import BoardScript
 
 
 if __name__ == '__main__':
-    my_board = BoardScript.Board(dimensions=10, obstacles=3, max_learning_value=100)
+    my_board = BoardScript.Board(dimensions=10, obstacles=3, disappearing_obstacles=True, bonuses=3, bonus_value=10, reward_value=100)
     my_board.initialize_board()
     my_board.draw_board()
+    while True:
+        x = input()
+        my_board.move(x)
+        my_board.draw_board()
+        print()
+        print(my_board.number_of_moves, my_board.total_score)
+        print()
