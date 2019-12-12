6ba51b4d0648f0ce4866d7965780671a1817f4fb
jake <jakubkuba997@gmail.com>
2019-12-12 18:56:18 +0100

    Created a board and assigned values to it

--- BoardScript.py
+++ BoardScript.py
@@ -1,13 +1,14 @@
 
 """
-    Create a playable maze and teach a reinforcement AI to play it using worth assignment
+    Create and draw a playable maze
 """
 
 
 class Board:
-    def __init__(self, dimensions: int = 10, obstacles: int = 2) -> None:
+    def __init__(self, dimensions: int = 10, obstacles: int = 2, max_learning_value: int = 100) -> None:
         self.dimensions = dimensions
         self.obstacles = obstacles
+        self.max_learning_value = max_learning_value
         self.board = []     # column, row, trophy, player, obstacle, worth
 
     def initialize_board(self) -> None:
@@ -34,7 +35,15 @@ class Board:
             while coordinates == trophy_coordinates or coordinates == player_coordinates or coordinates in obstacle_coordinates:
                 coordinates = [rd(0, self.dimensions - 1), rd(0, self.dimensions - 1)]
             obstacle_coordinates.append(coordinates)
-        print(trophy_coordinates, player_coordinates, obstacle_coordinates)
+        self.board[trophy_coordinates[1]][trophy_coordinates[0]][2] = 1
+        self.board[player_coordinates[1]][player_coordinates[0]][3] = 1
+        for coordinate in obstacle_coordinates:
+            self.board[coordinate[1]][coordinate[0]][4] = 1
 
     def actualise_worth(self) -> None:
-        pass
+        for row_index, row in enumerate(self.board):
+            for column_index, value in enumerate(row):
+                if value[2] == 1:
+                    self.board[row_index][column_index][5] = self.max_learning_value
+                elif value [4] == 1:
+                    self.board[row_index][column_index][5] = -self.max_learning_value
--- MainScript.py
+++ MainScript.py
@@ -8,6 +8,6 @@ import BoardScript
 
 
 if __name__ == '__main__':
-    my_board = BoardScript.Board(dimensions=10, obstacles=2)
+    my_board = BoardScript.Board(dimensions=10, obstacles=2, max_learning_value=100)
     my_board.initialize_board()
     print(my_board.board)
