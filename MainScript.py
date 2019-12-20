f07cd2303530f23af43a20cbf13a07752733b094
jake <jakubkuba997@gmail.com>
2019-12-20 19:54:10 +0100

    Actualized variables

--- MainScript.py
+++ MainScript.py
@@ -8,27 +8,38 @@ import EnvironmentScript
 
 
 class DeepIntelligence:
-    def __init__(self, episodes: int, show_every: int, debug: bool = True) -> None:
+    def __init__(self, debug: bool = True) -> None:
         self.debug = debug
-        self.env_episodes = episodes
         self.special_episode = True
-        self.show_every = show_every
+        # env variables
+        self.env_episodes = 1000
+        self.env_show_every = 50
+        self.env_move_penalty = 2
+        self.env_max_reward_value = 100
+        self.env_bonus_value = 10
+        # board variables
+        self.board_number_of_bonuses = 3
+        self.board_disappearing_obstacles = True
+        self.board_number_of_obstacles = 3
+        self.board_size_of_board = 10
 
     def train_singularly(self) -> None:
         for episode in range(self.env_episodes):
             if self.debug:
                 print('\tEpisode %i starting...\n' % episode)
             if self.special_episode:
-                game_1 = EnvironmentScript.Env(draw=True, dimensions=10, obstacles=3, disappearing_obstacles=True, bonuses=3, bonus_value=10, reward_value=100)
+                draw_this_one = True
             else:
-                game_1 = EnvironmentScript.Env(draw=False, dimensions=10, obstacles=3, disappearing_obstacles=True, bonuses=3, bonus_value=10, reward_value=100)
+                draw_this_one = False
+            game_1 = EnvironmentScript.Env(draw=draw_this_one, dimensions=self.board_size_of_board, obstacles=self.board_number_of_obstacles, disappearing_obstacles=self.board_disappearing_obstacles,
+                                           bonuses=self.board_number_of_bonuses, bonus_value=self.env_bonus_value, reward_value=self.env_max_reward_value)
             game_1.start_game()
-            if episode % self.show_every == 0:
+            if episode % self.env_show_every == 0:
                 self.special_episode = True
             else:
                 self.special_episode = False
 
 
 if __name__ == '__main__':
-    ai = DeepIntelligence(episodes=1000, show_every=50)
+    ai = DeepIntelligence()
     ai.train_singularly()
